4483798c987611e4d3ce6d43393e775334bbdbc0
Maxim <195960242+LizardVizard@users.noreply.github.com>
2025-10-28 20:45:43 +0200

    fix(prod): use dotenv only in development environment

--- db/pool.js
+++ db/pool.js
@@ -1,16 +1,22 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import { Pool } from "pg";
 
-dotenv.config();
-
 const isProduction = (process.env.APP_ENV || "").toLowerCase() === "production";
+const isDevelopment =
+  (process.env.APP_ENV || "").toLowerCase() === "development";
+
+if (!isProduction || isDevelopment) {
+  // import("dotenv").then((dotenv) => dotenv.config());
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
 
 export const CONNECTION_SETTINGS = {
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD,
-  ssl: isProduction,
+  ssl: isProduction ? { rejectUnauthorized: false } : true,
 };
 
 export default new Pool(CONNECTION_SETTINGS);
